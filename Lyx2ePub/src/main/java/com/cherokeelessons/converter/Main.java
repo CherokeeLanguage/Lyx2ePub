4a07c0cb60d144dd00b99f32830b701cc8e83f6f
mjoyner <mjoyner@vbservices.net>
2016-07-31 20:10:52 -0400

    activate ui

--- Lyx2ePub/src/main/java/com/cherokeelessons/converter/Main.java
+++ Lyx2ePub/src/main/java/com/cherokeelessons/converter/Main.java
@@ -1,11 +1,13 @@
 package com.cherokeelessons.converter;
 
+import java.awt.EventQueue;
+import java.io.IOException;
+
 import com.cherokeelessons.gui.MainWindow;
 import com.cherokeelessons.gui.MainWindow.Config;
 
 public class Main {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		MainWindow.Config config = new Config() {
 			@Override
 			public String getApptitle() {
@@ -17,6 +19,6 @@ public class Main {
 				return new MainApp(args);
 			}
 		};
+		EventQueue.invokeLater(new MainWindow(config, args));
 	}
-
 }
